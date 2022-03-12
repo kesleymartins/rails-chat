2f052f7ffbb5c2b59f92fadd0409f0aa06c53c77
kesleymartins <kesley.martins@vivaldi.net>
2022-03-12 20:28:43 -0300

    generate home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home', to: 'page#home'
+  get 'about', to: 'page#about'
+
   devise_for :users
   devise_scope :user do
     get 'users', to: 'devise/session#new'
