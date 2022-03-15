cee2b4a8f88a6b39fab39f9ae5f91a5345d37f95
kesleymartins <kesley.martins@vivaldi.net>
2022-03-14 23:45:26 -0300

    create a controller logic to create a room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,12 @@
 class RoomsController < ApplicationController
   def index
+    @room = Room.new
+
     @rooms = Room.all.public_rooms
     @users = User.all.all_except(current_user)
+  end
+
+  def create
+    @room = Room.create(name: params[:room][:name])
   end
 end
